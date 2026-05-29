d782fdec94a008ef47dc5566ce1b2dcbcab83afa
Jahn815 <pairtoll@gmail.com>
2026-05-28 22:30:10 -0400

    Made it so notes deleted if off-screen

--- new-game-project/new-game-project/note_script.gd
+++ new-game-project/new-game-project/note_script.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var note_speed = 100
+var note_speed = 500
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,4 +11,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	global_position.y += note_speed*delta #delta time!
+	
+	if (global_position.y > 1090):
+		queue_free() #deletes it and stuff after it goes out of screen.
+		#otherwise there could be hudnreds of notes off-screen which could lag it
 	pass
