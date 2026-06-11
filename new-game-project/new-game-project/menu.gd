50e32fc794bfae29cd3eb4370929e2bcfd81da3b
Jahn815 <pairtoll@gmail.com>
2026-06-10 21:02:51 -0400

    New particle gfx
    James cook pls

--- new-game-project/new-game-project/menu.gd
+++ new-game-project/new-game-project/menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var sau_play = $ShineasusualPlaybutton
 @onready var shiawase_play = $ShiawasePlaybutton
+@onready var monster_play = $MonsterPlayButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,4 +18,7 @@ func _process(delta: float) -> void:
 		GlobalTrackManager.selected_song = load("res://songs_mp3/Shiawase.mp3")
 		get_tree().change_scene_to_file("res://Stage.tscn")
 	#and then add more elifs as we add more songs
+	elif monster_play.is_pressed():
+		GlobalTrackManager.selected_song = load("res://songs_mp3/Monster.mp3")
+		get_tree().change_scene_to_file("res://Stage.tscn")
 	pass
